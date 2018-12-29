cbbdc87e2624940ddee2364142afceefe5848eb5
unknown <omarelewa21@gmail.com>
2018-12-29 18:51:55 +0200

    Make suggested project emhancements in SQL and README

--- README.md
+++ README.md
@@ -7,38 +7,59 @@ It works on some news database and extracts some information from it. These info
 * Days in which number error HTTP requests exceeds 1 percent.
 
 ## Quick start
+**Step1**: Setting up your environment. preferably, use FSND vagrant machine
 
-In your command window type
+##### vagrant installation
+* Install virtualBox which is the software that actually runs the virtual machine from this link 
 ```
-$ git clone https://github.com/omarelewa21/Movie_website
+https://www.virtualbox.org/wiki/Download_Old_Builds_5_1 
 ```
-Download newsdata.sql from this link 
+* Donlowad vagrant from this link 
+```
+https://www.vagrantup.com/downloads.html
+```
+* Download VM configuration by forking repository ``` https://github.com/udacity/fullstack-nanodegree-vm```  and cloning it on your device.
+* In your terminal, cd into **fullstack-nanodegree-vm** directory and you will find another directory called **vagrant**. 
+* Change directory to the **vagrant** directory.
+* In your terminal type command ```vagrant up``` to download the linux operating system and install it. 
+* After finishing, run ```vagrant ssh``` to log into your newly installed linux vm. 
+
+
+##### Install log analysis code
+* In your command window type
+```
+$ git clone https://github.com/omarelewa21/Log_analysis-
+```
+* From your VM 
+	Download newsdata.sql from this link 
 ``` 
 https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip 
-```
-either from a virtual machine or directly on your terminal type 
+``` 
+* Unzip the file and run command 
 ``` 
 $ psql -d news -f newsdata.sql
 ```
-You can now explore the database using command
+* You can now explore the database using command
 ```psql -d news```
 
-In you terminal run the python code by using command
+* In you terminal run the python code by using command
 ```
 $ python3 log_analysis.py
 ``` 
-### SQL code demo 
+
+### SQL code quick demo 
 SQL elements used are: 
-* Create view as: a method for creating temporary table in order to be used afterwards. 
 * select: Responsible for selecting distinct columns from tables. 
 * where: Used to put some condition for the selection process. 
 * count: Responsible for counting a partcular pattern in a column. 
 * group by: used with count to aggregate based on special condition 
-* having: used to specify some conditions after group by. 
-* ROW_NUMBER(): SQL function which gives each row a number in a tabe. 
 * inner join: method used to join two tables which have a column in common. 
-* sum: Gives the sum of all numbers in a cloumn.
 * order by: Responsible for arranging rows in a column in an acending or descending order. 
+
+###### Functions used are:
+* ROUND(): Function used to get decimal values from an operation or number.
+concat(): Function used to adjust a text or string by adding parts to it. 
+cast(): function used to segregate the date from the timestamp (used in the group by for this code).
 
 
 
--- log_analysis.py
+++ log_analysis.py
@@ -1,4 +1,5 @@
-''' This file is working on a news database in the aim of
+#!/usr/bin/env python3
+'''This file is working on a news database in the aim of
     doing some analysis on these data to obtain information.
     news database contains three tables of information as following:
     authors table: has 3 columns (name, bio, id)
@@ -11,116 +12,79 @@ import psycopg2
 DBNAME = "news"
 
 
-def top_three_articles():
+def top_three_articles(cur):
     """Prints the top three article on views"""
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-    c.execute('''create view counted_views as
-                    select path, count(path) as top_counts from log
-                    group by path
-                    having path like '%candidate_is_jerk%' or
-                            path like '%bears_love_berries%' or
-                            path like '%bad_things_gone%' or
-                            path like '%goats_eat_google%' or
-                            path like '%trouble_for_trouble%' or
-                            path like '%balloon_goons_doomed%' or
-                            path like '%media_obsessed_with_bears%' or
-                            path like '%so_many_bears%'
-                    order by count(path) desc limit 8;
-                    create view n_articles as
-                        select *, ROW_NUMBER() over (order by title) as t_id
-                        from articles;
-                    create view n_counted_views as
-                        select *, ROW_NUMBER() over (order by path) as t_id
-                        from counted_views;
-                    select n_articles.title, n_counted_views.top_counts
-                    from n_articles inner join n_counted_views
-                    on n_articles.t_id = n_counted_views.t_id
-                    order by top_counts desc;''')
+    cur.execute('''select articles.title, counted_views.top_counts
+                        from articles
+                        inner join (select path, count(path) as top_counts
+                                    from log
+                                    group by path
+                                    order by count(path) desc limit 9)
+                                        as counted_views
+                        on counted_views.path =
+                            concat('/article/', articles.slug)
+                        order by top_counts desc limit 3;''')
 
-    posts = c.fetchall()
-    db.close()
-    counts = 0
-    while counts < 3:
-        print(posts[counts][0] + ', ' + str(posts[counts][1]) + ' views')
-        counts += 1
-    return None
-
-
-def listed_authors():
-    """Prints all authors in decending order based on views"""
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-    c.execute('''create view counted_views as
-                    select path, count(path) as top_counts from log
-                    group by path
-                    having path like '%candidate_is_jerk%' or
-                            path like '%bears_love_berries%' or
-                            path like '%bad_things_gone%' or
-                            path like '%goats_eat_google%' or
-                            path like '%trouble_for_trouble%' or
-                            path like '%balloon_goons_doomed%' or
-                            path like '%media_obsessed_with_bears%' or
-                            path like '%so_many_bears%'
-                    order by count(path) desc limit 8;
-                    create view n_articles as
-                        select *, ROW_NUMBER() over (order by title) as t_id
-                        from articles;
-                    create view n_counted_views as
-                        select *, ROW_NUMBER() over (order by path) as t_id
-                        from counted_views;
-                    create view joining_2ns as
-                            select n_articles.title, n_articles.author,
-                                n_counted_views.top_counts
-                                from n_articles inner join n_counted_views
-                                on n_articles.t_id = n_counted_views.t_id;
-                select authors.name, sum(joining_2ns.top_counts)
-                     as authors_views
-                from authors
-                inner join joining_2ns on
-                    authors.id = joining_2ns.author
-                group by authors.name
-                order by authors_views desc;  ''')
-
-    data = c.fetchall()
-    db.close()
-    for row in list(data):
+    data = cur.fetchall()
+    for row in data:
         print(row[0] + ' -- ' + str(row[1]) + ' views')
     return None
 
 
-def margin_one_percent():
-    '''Prints days in which more than 1 percent of requests lead to error'''
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-    e = db.cursor()
-    c.execute('''select count(status) as total_number from log
-                        group by cast(log.time as date); ''')
-    e.execute('''select count(status) as total_number from log
-                        where status = '404 NOT FOUND'
-                        group by cast(log.time as date); ''')
-    total_data = c.fetchall()
-    error_data = e.fetchall()
-    counter = 0
-    while counter < 31:
-        error_percentage = error_data[counter][0]/total_data[counter][0] * 100
-        if error_percentage > 1:
-            print(
-                "July " + str(counter+1) + ",2016 -- " +
-                str(error_percentage) + "% errors"
-                )
-        counter += 1
+def listed_authors(cur):
+    """Prints all authors in decending order based on views"""
+    cur.execute('''select authors.name, count(log_articles.path) as popular
+                        from authors
+                        inner join (select articles.author, log.path
+                                    from articles
+                                    inner join log
+                                    on log.path =
+                                        concat('/article/', articles.slug))
+                                        as log_articles
+                            on authors.id = log_articles.author
+                    group by authors.name
+                    order by popular desc;
+                    ''')
+
+    data = cur.fetchall()
+    for row in (data):
+        print(row[0] + ' -- ' + str(row[1]) + ' views')
     return None
 
 
-def Answers():
-    '''Displaying all above function in organized way'''
-    print("Top three articles are:\n")
-    top_three_articles()
-    print("\n\nMost popular authors are:\n")
-    listed_authors()
-    print('''\n\nDays that have more than 1 percent of requests
-             lead to errors are:\n''')
-    margin_one_percent()
+def margin_one_percent(cur):
+    '''Prints days in which more than 1 percent of requests lead to error'''
+    cur.execute('''select date, error_percentage from
+                (select A1.date,
+                    (100 / (ROUND(A1.total_number / A2.error_number, 2)))
+                        as error_percentage
+                    from
+                        (select cast(log.time as date) as date, count(status)
+                        as total_number from log
+                        group by cast(log.time as date)) as A1
+                    inner join
+                        (select cast(log.time as date) as date, count(status)
+                        as error_number from log
+                        where status = '404 NOT FOUND'
+                        group by cast(log.time as date)) as A2
+                        on A1.date = A2.date) as A
+                    where error_percentage > 1''')
+    data = cur.fetchall()
+    for row in data:
+        print(str(row[0]) + ' -- ' + str(row[1]))
+    return None
 
-Answers()
+
+if __name__ == "__main__":
+    '''Print data in an organized manner'''
+    db = psycopg2.connect('dbname=news')
+    cur = db.cursor()
+    cur2 = db.cursor()
+    print("Top three articles are:")
+    top_three_articles(cur)
+    print("\n\nMost popular authors are:")
+    listed_authors(cur)
+    print(
+        "\n\nDays that have more than 1 percent of requests " +
+        "lead to errors are: ")
+    margin_one_percent(cur)
